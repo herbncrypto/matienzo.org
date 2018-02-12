f6bb6bdecb3efe09bfd1bd5e3b0464b35e213f2e
Mark A. Matienzo <matienzo@stanford.edu>
2018-02-12 10:58:56 -0800

    Add lambda to ping Superfeedr

---
+++ _functions/deploy-succeeded.js
@@ -0,0 +1,18 @@
+const request = require('request');
+
+exports.handler = function(event, context, callback) {
+  console.log("event received:", event);
+  console.log("[superfeedr] preparing to ping")
+  request.post(
+    `http://anarchivist.superfeedr.com/?hub.mode=publish&hub.url=https://matienzo.org/*`,
+    function (error, response, body){
+      if (!error && response.statusCode == 204) {
+        console.log("[superfeedr] ping successful");
+        callback(null, {statusCode: 204});
+      } else {
+        console.log("[superfeedr] ping failed:", error);
+        callback(error, {statusCode: response.statusCode});
+      }
+    }
+  );
+}
