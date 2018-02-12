8adb876890325dfedcf298a6b301441e2255e804
Mark A. Matienzo <matienzo@stanford.edu>
2018-02-12 11:49:51 -0800

    basdfjsd

--- _functions/deploy-succeeded.js
+++ _functions/deploy-succeeded.js
@@ -2,7 +2,7 @@ const request = require('request');
 
 exports.handler = function(event, context, callback) {
   var e = JSON.parse(event.body);
-  console.log("event received:\n", e.keys)
+  console.log("event received:\n", e.keys())
   console.log("[superfeedr] preparing to ping")
   request.post(
     `http://anarchivist.superfeedr.com/?hub.mode=publish&hub.url=https://matienzo.org/*`,
