340ab99b30713dc96c27ea0904021e6d85d97ccd
Mark A. Matienzo <matienzo@stanford.edu>
2018-02-12 12:24:22 -0800

    revert to previous version

--- _functions/deploy-succeeded.js
+++ _functions/deploy-succeeded.js
@@ -2,23 +2,19 @@ const request = require('request');
 
 exports.handler = function(event, context, callback) {
   var e = JSON.parse(event.body);
-  // if process.env.SUPERFEEDR_USERNAME {
-    if e.payload.context == "production" {
-      console.log("[superfeedr] preparing to ping")
-      request.post(
-        `http://anarchivist.superfeedr.com/?hub.mode=publish&hub.url=${e.payload.url}/*`,
-        function (error, response, body){
-          if (!error && response.statusCode == 204) {
-            console.log("[superfeedr] ping successful");
-            callback(null, {statusCode: 204});
-          } else {
-            console.log("[superfeedr] ping failed:", error);
-            callback(error, {statusCode: response.statusCode});
-          }
+  if e.payload.context == "production" {
+    console.log("[superfeedr] preparing to ping")
+    request.post(
+      `http://anarchivist.superfeedr.com/?hub.mode=publish&hub.url=https://matienzo.org/*`,
+      function (error, response, body){
+        if (!error && response.statusCode == 204) {
+          console.log("[superfeedr] ping successful");
+          callback(null, {statusCode: 204});
+        } else {
+          console.log("[superfeedr] ping failed:", error);
+          callback(error, {statusCode: response.statusCode});
         }
-      );
-    }
-  // } else {
-  //   callback("Superfeedr username is not set", {statusCode: 500});
-  // }
+      }
+    );
+  }
 }
